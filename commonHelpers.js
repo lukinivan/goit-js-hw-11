18b75e04608a3df6908fbf18cd1ffbf50eecc309
lukinivan <Lukinivan09@gmail.com>
2024-05-22 23:03:14 +0000

    Deploying to gh-pages from @ lukinivan/goit-js-hw-11@c05c1c2b3d8d118164151b7929043aa015b0a801 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,26 @@
-import{a as s}from"./assets/iziToast-0d52f805.js";import{f as c}from"./assets/vendor-77e16229.js";let a;const u={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){if(t[0]-Date.now()<0)return n.disabled=!0,s();a=t[0],n.disabled=!1}};c("#datetime-picker",u);document.addEventListener("DOMContentLoaded",()=>n.disabled=!0);const d=document.querySelector("#datetime-picker"),n=document.querySelector("[data-start]"),i=document.querySelector("[data-days]"),l=document.querySelector("[data-hours]"),m=document.querySelector("[data-minutes]"),f=document.querySelector("[data-seconds]");n.addEventListener("click",()=>{y(),n.disabled=!0,d.disabled=!0});function y(){r(a-Date.now());const t=setInterval(()=>{const e=a-Date.now();r(e),e<=1e3&&(clearInterval(t),d.disabled=!1)},1e3)}function r(t){const e=h(t);i.textContent=e.days,l.textContent=e.hours,m.textContent=e.minutes,f.textContent=e.seconds}function h(t){return{days:o(Math.floor(t/864e5)),hours:o(Math.floor(t%864e5/36e5)),minutes:o(Math.floor(t%864e5%36e5/6e4)),seconds:o(Math.floor(t%864e5%36e5%6e4/1e3))}}function o(t){return`${t}`.padStart(2,"0")}
+import{i as c,S as u}from"./assets/vendor-0fc460d7.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();function d(s){return s.map(p).join("")}function p(s){return`
+  <div class="image-container">
+    <a href="${s.largeImageURL}">
+      <img src="${s.webformatURL}" alt="${s.tags}" title="" />
+    </a>
+    
+    <div class="stats">
+      <div class="stat">
+        <span class="label">Likes</span>
+        <span class="value">${s.likes}</span>
+      </div>
+      <div class="stat">
+        <span class="label">Views</span>
+        <span class="value">${s.views}</span>
+      </div>
+      <div class="stat">
+        <span class="label">Comments</span>
+        <span class="value">${s.comments}</span>
+      </div>
+      <div class="stat">
+        <span class="label">Downloads</span>
+        <span class="value">${s.downloads}</span>
+      </div>
+    </div>
+  </div>`}function m(s){const r="https://pixabay.com/api/",a="44003480-04272ce3fae5ad0292fb853b7",o=new URLSearchParams({key:a,q:s,image_type:"photo",orientation:"horizontal",safesearch:"true"}),e=`${r}?${o}`;return fetch(e).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}const f="/goit-js-hw-11/assets/blocked-295d98f9.svg";function g(){c.show({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"black",color:"red",position:"topRight",timeout:3e3,close:!0,progressBar:!0,iconUrl:f})}const h=new u(".gallery a",{captionsData:"alt",captionDelay:250}),y=document.querySelector(".js-request-form"),i=document.querySelector(".js-gallery"),l=document.querySelector(".js-loader");y.addEventListener("submit",s=>{s.preventDefault();const r=s.currentTarget.elements.query.value.trim();i.innerHTML="",l.classList.add("loader"),m(r).then(a=>{a.total===0?g():i.innerHTML=d(a.hits),h.refresh(),l.classList.remove("loader")}),s.currentTarget.reset()});
 //# sourceMappingURL=commonHelpers.js.map
